4870a96e4f44a5f1300222c9a5007b392dafafca
AloeThron <lxe.tr.deat@gmail.com>
2024-01-23 15:35:37 +0700

    setup

---
+++ components/ui/route.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import clsx from "clsx";
+
+interface routeProps {
+  route: string;
+  label: string;
+  isActive?: boolean;
+  onClick?: () => void;
+}
+
+export default function Route({ route, label, isActive, onClick }: routeProps) {
+  return (
+    <Link
+      href={route}
+      onClick={onClick}
+      className={clsx(isActive && "text-primary")}
+    >
+      {label}
+    </Link>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '346.8 77.2% 49.8%',
+        secondary: '240 4.8% 95.9%',
+        tertiary: '#403d39',
+        light: '#fffcf2',
       },
     },
   },
