04609112ab0e3f4873accd1de6caac6d17b43240
AloeThron <lxe.tr.deat@gmail.com>
2024-02-08 00:28:27 +0700

    edit file

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,23 +1,21 @@
-import NextAuth, {AuthOptions} from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import {db} from "@/lib/database"
+import { db } from "@/lib/database";
 
-
-
-export const authOptions: AuthOptions = ({
-  adapter: PrismaAdapter(db),
+const authOptions: AuthOptions = {
+  // adapter: PrismaAdapter({ ...db, ...{ adapter: "prisma" } }),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
-})
+};
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
