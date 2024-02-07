f7b813d666a162264856d3bf56ad231c2484d8de
AloeThron <lxe.tr.deat@gmail.com>
2024-02-08 01:04:32 +0700

    fix file

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,19 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/database";
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter({ ...db, ...{ adapter: "prisma" } }),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+}
+
+export default authOptions;
